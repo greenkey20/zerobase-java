7a130ecc5c39d12e101874b57d46194b253d1fec
Eunyoung KANG <greenkey20@gmail.com>
2022-08-21 23:59:44 +0900

    feat: add study codes of conditions

---
+++ exercises/src/JavaJeongseok/exercises/ch4/Ex4_14.java
@@ -0,0 +1,18 @@
+package exercises.src.JavaJeongseok.exercises.ch4;
+
+import java.util.Scanner;
+
+// 2022.8.21(일) 23h50
+public class Ex4_14 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String inputValue = "";
+
+        do {
+
+        } while (!inputValue.equals("종료"));
+
+    }
+}
