b53a72c5912231df327c68faefdbd7bf1681a74b
Eunyoung KANG <greenkey20@gmail.com>
2022-08-22 23:55:50 +0900

    fix: update an exercise of conditions

--- exercises/src/JavaJeongseok/exercises/ch4/Ex4_14.java
+++ exercises/src/JavaJeongseok/exercises/ch4/Ex4_14.java
@@ -2,17 +2,38 @@ package exercises.src.JavaJeongseok.exercises.ch4;
 
 import java.util.Scanner;
 
+import static exercises.src.JavaJeongseok.exercises.common.MenuGuide.*;
+
 // 2022.8.21(일) 23h50
 public class Ex4_14 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int answer = (int)(Math.random() * 100) + 1; // 1~100 사이의 임의의 값을 얻어서 answer에 저장
 
-        String inputValue = "";
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+
+        int count = 0;
 
         do {
+            count++;
+            displayMenuGuide();
 
-        } while (!inputValue.equals("종료"));
+            input = scanner.nextInt();
 
+            if (input < answer) {
+                System.out.println("더 큰 수를 입력하세요");
+            } else if (input > answer) {
+                System.out.println("더 작은 수를 입력하세요");
+            } else {
+                System.out.println("맞췄습니다! 시도 회수는 " + count + "번입니다");
+            }
+        } while (input != 0);
+
+    }
+
+    public static void displayMenuGuide() {
+        System.out.println("== 프로그램을 종료하려면 '0'을 입력하세요 ==");
+        System.out.print("1~100 사이의 정수를 1개 입력해 주세요 > ");
     }
 }
