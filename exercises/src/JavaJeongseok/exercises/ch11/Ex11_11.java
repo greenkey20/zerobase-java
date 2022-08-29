64c6629f07a285459880d6ce1c0035d4a16af9b3
Eunyoung KANG <greenkey20@gmail.com>
2022-08-29 23:59:38 +0900

    feat: add a Java collection framework exercise

---
+++ exercises/src/JavaJeongseok/exercises/ch11/Ex11_11.java
@@ -0,0 +1,10 @@
+package exercises.src.JavaJeongseok.exercises.ch11;
+
+// 2022.8.29(월) 23h50
+public class Ex11_11 {
+    public static void main(String[] args) {
+        public int hashCode() {
+            return toString().hashCode();
+        }
+    }
+}
