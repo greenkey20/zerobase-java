ccfa5a74a14b6d1422d4374d09547fe4dc402c78
Eunyoung KANG <greenkey20@gmail.com>
2023-08-13 18:12:55 +0900

    feat: add exception handling study codes

---
+++ exercises/src/jumptojava/exception/Sample1.java
@@ -0,0 +1,23 @@
+package exercises.src.jumptojava.exception;
+
+// 2023.8.13(일) 18h
+public class Sample1 {
+    public void sayNickname(String nickname) {
+        if(nickname.equals("바보")) {
+//            return;
+            throw new FoolException(); // 적극적으로 예외 발생시킴
+        }
+
+        System.out.println("당신의 별명은 " + nickname + "입니다");
+    }
+
+    public static void main(String[] args) {
+        Sample1 sample1 = new Sample1();
+        sample1.sayNickname("바보");
+        sample1.sayNickname("초록꼬마");
+    }
+}
+
+class FoolException extends RuntimeException {
+
+}
